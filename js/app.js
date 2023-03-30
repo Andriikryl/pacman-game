9d067e0bf170836339a3e8e42cdcd0a2d4444867
Amdrii <andreyk557@gmail.com>
2023-03-30 19:46:31 +0300

    feat[images]

--- js/app.js
+++ js/app.js
@@ -8,15 +8,17 @@ canvas.height = innerHeight;
 class Boudary {
   static width = 40;
   static height = 40;
-  constructor({ position }) {
+  constructor({ position, image }) {
     this.position = position;
     this.width = 40;
     this.height = 40;
+    this.image = image;
   }
 
   draw() {
-    c.fillStyle = "blue";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    // c.fillStyle = "blue";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
@@ -86,6 +88,7 @@ map.forEach((row, i) => {
         boundaries.push(
           new Boudary({
             position: { x: Boudary.width * j, y: Boudary.height * i },
+            image: image,
           })
         );
         break;
